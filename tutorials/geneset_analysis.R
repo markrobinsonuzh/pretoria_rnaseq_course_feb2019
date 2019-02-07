f8a2dd14c1e1d097b56bc9d3e81eea5351f35035
markrobinsonuzh <mark.robinson@imls.uzh.ch>
2019-02-07 06:20:32 +0100

    updated geneset script

--- tutorials/geneset_analysis.R
+++ tutorials/geneset_analysis.R
@@ -1,44 +1,55 @@
 
-# load packages
+## load packages
 library(limma)
 library(msigdbr)
 
-# load data
+## load data
 d <- readRDS("edgeR_object.rds")
 
+# explore design matrix / contrast / data
 (design <- d$design)
 
-contrast <- makeContrasts("TRT2-CTRL",
-                          levels = colnames(d$design))
-contrast
+(contrast <- makeContrasts("TRT2-CTRL",
+                          levels = colnames(d$design)))
 
+d$samples
+dim(d)
 
-m_df = msigdbr(species = "Mus musculus") %>%
-  filter(gs_cat %in% c("H", "C5", "C7"))
+## do DE analysis with voom
+v <- voom(d, design, plot = TRUE)
+
+f <- lmFit(v, design)
+f <- eBayes(f) # moderate variance
+
+cf <- contrasts.fit(f, contrast)
+cf <- eBayes(cf)
+
+topTable(cf)
+
+# do a sanity check
+barplot(v$E["ENSMUSG00000035385.Ccl2",], las=2)
+
+# get gene sets from MSIGDB and take the HALLMARK sets
+m_df <- msigdbr(species = "Mus musculus")
+keep <- m_df$gs_cat=="H"
+m_df <- m_df[keep,]
 
 sets <- split(m_df$gene_symbol, m_df$gs_name)
-n <- sapply(sets, length)
-sets <- sets[n >= 20 & n < 1000]
 length(sets)
 
+# match up the gene names from genesets to dataset
+inds <- ids2indices(sets, d$genes$symbol,
+                    remove.empty=TRUE)
 
-  inds <- ids2indices(sets, d$genes$symbol,
-                      remove.empty=TRUE)
-
-  v <- voom(d, design, plot = TRUE)
-  f <- lmFit(v, design)
-  f <- eBayes(f)
-  cf <- contrasts.fit(f, contrast)
-  cf <- eBayes(cf)
-
-
-  cm <- camera(v, inds, design=design,
-               contrast=contrast)
-
-
+## Run 'camera'
+cm <- camera(v, inds, design=design,
+             contrast=contrast)
+  
+  
+# pull out t-statistics, make 'barcodeplot'
 tstat <- cf$t[,1]
 indices <- inds[["HALLMARK_INTERFERON_GAMMA_RESPONSE"]]
 
-barcodeplot(tstat, index = indices, cex.main=.8,
-              quantiles=c(-1,1)*qt(.95, df=6))
+barcodeplot(tstat, index = indices,
+            quantiles=c(-1,1)*qt(.95, df=6))
 
